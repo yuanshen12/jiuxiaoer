a7f40087a64fc1a7e3071286b108ce7d1db44476
yuanshen12 <53459718+yuanshen12@users.noreply.github.com>
2020-02-14 17:09:30 +0800

    定位封装

--- element/call_element.py
+++ element/call_element.py
@@ -13,3 +13,9 @@ class Element(object):
 
     def swipe(self, start_x, start_y, end_x, end_y, duration):  # duration定义滑动时间参数，毫秒单位
         return self.driver.swipe(start_x, start_y, end_x, end_y, duration)
+
+    def amend(self, one, two):
+        name = one
+        names = name[1].format(two)
+        name_to = (name[0],) + (names,)
+        return name_to

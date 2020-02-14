47fee40039adc47e3d1eb097e12de8481e8a68af
yuanshen12 <53459718+yuanshen12@users.noreply.github.com>
2020-02-14 11:15:49 +0800

    发现页面

--- wechat/discover/call_discover.py
+++ wechat/discover/call_discover.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from wechat.common.call_wechat import wechat
-from wechat.common.call_common import Login
-from wechat.operation.home.call_home import Home
+from common.call_wechat import wechat
+from common.call_common import Login
+from wechat.home.call_home import Home
 from selenium.webdriver.common.touch_actions import TouchActions
 
 
@@ -14,19 +14,61 @@ class Discover(Login):
     call_price = (By.CLASS_NAME, "price")  # 发现页面商品价格
     call_add = (By.CLASS_NAME, "buy-btn-jia-logo")  # 发现页面商品加号
     call_side = (By.CLASS_NAME, "middle-body")  # 发现页面商品反转点击
+    call_cart = (By.CLASS_NAME, "float_cart")  # 发现页面点开购物车
 
     def discover_title(self, num):  # 发现页面选择标题栏
+        self.login()
+        Home(driver).home_menu(1)
         age = self.wait(EC.presence_of_all_elements_located, self.call_header)[num]
         TouchActions(self.driver).tap(age).perform()
 
-    def discover_img(self, one):  # 发现页商品图片点击
+    # 发现页面点击或者返回（stat选择打开位置，img点开图片，title点开标题,sub点开小标题,price返回价格）
+    def discover_item(self, stat=0, img=0, title=1, sub=0, price=0):
         self.login()
         Home(driver).home_menu(1)
-        names = self.wait(EC.presence_of_all_elements_located, self.call_img)[one]
-        print(names.text)
+        if stat == 0:  # 选择点击图片
+            while True:
+                try:
+                    self.wait(EC.presence_of_all_elements_located, self.call_img)[img].click()
+                    self.swipe(600, 1200, 600, 600, 500)
+                    break
+                except:
+                    pass
+        if stat == 1:  # 选择点开标题
+            while True:
+                try:
+                    self.wait(EC.presence_of_all_elements_located, self.call_title)[title].click()
+                    self.swipe(600, 1200, 600, 600, 500)
+                    break
+                except:
+                    pass
+        else:
+            if stat == 2:  # 选择点开小标题
+                while True:
+                    try:
+                        self.wait(EC.presence_of_all_elements_located, self.call_sub_title)[sub].click()
+                        self.swipe(600, 1200, 600, 600, 500)
+                        break
+                    except:
+                        pass
+            elif stat == 3:  # 选择返回价格
+                while True:
+                    try:
+                        prices = self.wait(EC.presence_of_all_elements_located, self.call_price)[price].text
+                        self.swipe(600, 1200, 600, 600, 500)
+                        return prices
+                    except:
+                        pass
+            else:
+                self.wait(EC.element_to_be_clickable, self.call_add).click()
+
+    def discover_cart(self):  # 发现页面点开购物车
+        self.login()
+        Home(driver).home_menu(1)
+        self.wait(EC.element_to_be_clickable, self.call_cart).click()
 
 
 if __name__ == "__main__":
     driver = wechat()
     name = Discover(driver)
-    name.discover_img(1)
+    name.discover_cart()
